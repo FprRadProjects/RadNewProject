301f1e8202f4cfa55881cafcaa53bc80336dd4ea
unknown <k.kazemnasab@gmail.com>
2019-02-19 09:32:30 +0330

    xcxvxvfff

---
+++ src/locales/forms.translations.js
@@ -0,0 +1,17 @@
+export const formTranslations = {
+    fa: {
+        //--------------نام فرم ها--------------شروع
+        home: 'صفحه اصلی',
+        work_dashboard: 'کارتابل کار',
+        references_dashboard: 'کار تابل ارجاعات',
+        //--------------نام فرم ها--------------پایان
+    },
+    en: {
+        //Start-------------- FORM NAME --------------
+        home: 'Home',
+        work_dashboard: 'Work Dashboard',
+        references_dashboard: 'References Dashboard',
+        //End-------------- FORM NAME --------------
+
+    },
+}
