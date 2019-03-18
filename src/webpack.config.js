aec426cbe3b8500e6e0e6b725f6fd32cc7813f6c
unknown <k.kazemnasab@gmail.com>
2019-03-18 09:46:37 +0330

    sss

--- src/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const Config={
-    'Config':JSON.stringify(process.env.NODE_ENV==='production'?{
-        BaseUrl:"185.189.120.190:2535"
-    }:{
-        BaseUrl:"192.168.1.44:2535"
-
-    })
-}
-module.exports=Config;
