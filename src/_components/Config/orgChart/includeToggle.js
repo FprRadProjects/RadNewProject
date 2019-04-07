5a503d90bb5e7fa24a7b6c45002796cbdb1da473
ehsanhosseinzadehazad <ehsan.hsnzd@gmail.com>
2019-04-07 19:04:55 +0430

    clear consolelog

--- src/_components/Config/orgChart/includeToggle.js
+++ src/_components/Config/orgChart/includeToggle.js
@@ -3,7 +3,7 @@ export function toggleColorAll(cname,color){
     const  elements = document.getElementsByClassName(cname);
     for (var i = 0; i < elements.length; i++) {
         elements[i].setAttribute('stroke', color);
-        console.log(elements[i])
+
     }
 
 }
