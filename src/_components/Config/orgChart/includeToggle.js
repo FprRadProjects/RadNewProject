67445ef29779e4555a2717c167f6120345804fde
ehsanhosseinzadehazad <ehsan.hsnzd@gmail.com>
2019-04-07 15:37:49 +0430

    diag

---
+++ src/_components/Config/orgChart/includeToggle.js
@@ -0,0 +1,9 @@
+export function toggleColorAll(cname,color){
+
+    const  elements = document.getElementsByClassName(cname);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute('stroke', color);
+        console.log(elements[i])
+    }
+
+}
